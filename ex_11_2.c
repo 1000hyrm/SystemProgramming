fbc16624f6d6608a8079d18dd500b40e7dd8e1e1
1000hyrm <90028687+1000hyrm@users.noreply.github.com>
2022-06-14 14:22:56 +0900

    Update ex_11_2.c

--- ex_11_2.c
+++ ex_11_2.c
@@ -1,3 +1,13 @@
+//argv[1]으로 시간 (초 단위) 값이 입력되면, 해당 시간이 경과하면, 화면에 “Timeout”이라고 출력하고 종료하는 프로그램
+/* - 단, 프로그램이 수행되는 도중에 사용자가 입력하는 control-C는 무시된다.
+   - alarm(2) 을 사용하여 프로그래밍
+   - 문자를 숫자로 변경하는 atoi 함수 이용 */
+/*실행 예:
+% a.out 5
+// 5초가 경과
+Timeout
+% */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
