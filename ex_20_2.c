3814c09c533e3f8b236875ae23892c5f6f609d3e
1000hyrm <90028687+1000hyrm@users.noreply.github.com>
2022-06-14 16:30:51 +0900

    Update ex_20_2.c

--- ex_20_2.c
+++ ex_20_2.c
@@ -1,3 +1,15 @@
+/* Shell에서 “% a.out command option fileName” 실행하면, 
+- command를 실행하고, 
+- command가 stdout으로 출력하는 내용이 fileName에 저장되도록 하는 프로그램.
+- 예를 들면, 다음과 같이 실행하면
+% a.out /bin/date output
+- /bin/date의 실행 결과가 stdout으로 출력되지 않고, “output”이라는 이름의 파일에 저장된다. 
+- “option”은 없을 수 있다.
+- “option”이 있는 경우, 맨 마지막 argument가 출력이 저장될 파일 이름이다.
+- 예를들면, “% a.out /bin/ls -a -l output” 처럼 실행되면, “output”이 저장될 파일 이름이 된다.
+- 이때, “output” 파일이 있으면, 파일에 있는 내용이 전부 삭제되도록 한다.
+- 파일이 없는 경우 파일이 생성되며, 생성되는 파일의 mode는, 사용자는 read/write, 그룹/others 모두 read/execute만이 허용되도록 한다. (umask는 000이라고 가정한다.) */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
