6ecda4c26deee2be16c7be1687e4183111c676c1
1000hyrm <90028687+1000hyrm@users.noreply.github.com>
2022-06-14 16:32:47 +0900

    Update ex_21_3.c

--- ex_21_3.c
+++ ex_21_3.c
@@ -29,9 +29,6 @@ void *func(void *arg){
 	val = atoi(s);
 	val = val*val;
 	return ((void *) val);
-	//return ((void *) ret);
-	//return (ret); //쓰레기값?
-	//return ((void) *ret); //error
 }
 
 int main(int argc, char *argv[]){
@@ -54,42 +51,3 @@ int main(int argc, char *argv[]){
 	printf("thread return %d\n", res);
 	exit(EXIT_SUCCESS);
 }
-
-/*
-void *func(void *arg){
-	int *ret;
-	printf("thread() argument with %s\n", (char *)arg);
-	if((ret = (int *)malloc(20))==NULL){
-		perror("malloc error");
-		exit(2);
-	}
-	int i = atoi(arg);
-	printf("i = %d\n", i);
-	
-	ret = &i;
-	printf("ret = %p\n", ret);
-	printf("i = %p\n", &i);
-	printf("*ret = %d\n", *ret);
-	pthread_exit(ret);
-}
-
-void main(int argc, char *argv[]){
-	pthread_t handle;
-	char line[256];
-	int s, res;
-	void *rt;
-	
-	if(pthread_create(&handle, NULL, func, argv[1]) != 0){
-		perror("pthread_create() error");
-		exit(1);
-	}
-	printf("Thread created successfully\n");
-	
-	if(pthread_join(handle, &rt)!=0){
-		perror("pthread_join() error");
-		exit(3);
-	}
-	
-	printf("thread existed with %ls\n", (int *)rt);
-	free(rt);
-}*/
