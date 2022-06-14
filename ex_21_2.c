81936332a13e60879376ffaa28011890273d6189
1000hyrm <90028687+1000hyrm@users.noreply.github.com>
2022-06-14 16:31:43 +0900

    Update ex_21_2.c

--- ex_21_2.c
+++ ex_21_2.c
@@ -1,3 +1,6 @@
+/* Child 프로세스를 생성하여 child 프로세스가 “/bin/date -u” 명령을 수행하도록 하는 프로그램.
+이때, parent 프로세스는 child 프로세스가 종료할 때까지 기다린 후, child가 종료하면 child의 exit 값을 10진수로 출력하고 child pid를 10진수로 출력한다.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
