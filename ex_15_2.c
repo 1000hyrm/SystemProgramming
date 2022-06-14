02636d76ee3f5230bc9e04518f12f549efa6fdd8
1000hyrm <90028687+1000hyrm@users.noreply.github.com>
2022-06-14 14:26:39 +0900

    Update ex_15_2.c

--- ex_15_2.c
+++ ex_15_2.c
@@ -1,3 +1,22 @@
+
+//무한 loop를 돌면서 사용자가 키보드로 입력한 내용을 argv[1] 파일로 출력하고, argv[2] 시간이 지나면 종료되는 프로그램
+/* ■ 사용자가 키보드로 입력한 모든 내용은 argv[1] 파일에 저장된다.
+   ■ 프로그램이 수행되는 도중에 사용자가 control-C (SIGINT)를 입력하면, 종료되지 않고 이전의 동작을 계속 수행한다.
+   ■ 프로그램이 수행되는 도중에 사용자가 control-\ (SIGQUIT)를 입력하면, 종료되지 않고 이전의 동작을 계속 수행한다.
+   ■ argv[2]의 시간만큼 지나면, control-C와 control-\이 입력된 횟수를 stdout으로 출력하고 종료한다. */
+/* 실행 예
+[root@localhost]# ./a.out test 10
+Hello World // 사용자가 입력
+^C^C^C
+^\^\^\^\^\
+Time out!! // 10초 경과
+SIGINT : 3
+SIGQUIT : 5
+[root@localhost]# cat test
+Hello World
+[root@localhost]#
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
