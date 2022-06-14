95b16bbf5d82501dd0c6b33d1a955ddcc25a8c89
1000hyrm <90028687+1000hyrm@users.noreply.github.com>
2022-06-14 16:25:43 +0900

    Update ex_16_4.c

--- ex_16_4.c
+++ ex_16_4.c
@@ -1,3 +1,16 @@
+
+// child process는 argv[2] 실행 파일을 임의의 개수의 argument (argv[3], argv[4], ..., argv[n])로 실행하며, 그 결과는 pipe로 출력된다.
+// parent는 pipe로부터 읽은 데이터를 argv[1] 파일에 저장한다.
+/*실행 예
+% a.out outfile /bin/ls -l
+// “/bin/ls –l”을 실행하여, 그 결과를 outfile에 저장
+% a.out outfile /bin/ls -l -a
+// “/bin/ls –l -a”을 실행하여, 그 결과를 outfile에 저장
+% a.out outfile /bin/ls -a -C -F
+// “/bin/ls –a -C -F”을 실행하여, 그 결과를 outfile에 저장
+% a.out outfile /bin/date -u
+// “/bin/date –u”을 실행하여, 그 결과를 outfile에 저장*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
