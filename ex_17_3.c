c93ea81ccc25e1f204b8ff7cd0aeb76b0a6b8bb1
1000hyrm <90028687+1000hyrm@users.noreply.github.com>
2022-06-14 16:27:24 +0900

    Update ex_17_3.c

--- ex_17_3.c
+++ ex_17_3.c
@@ -1,3 +1,8 @@
+/*  main thread는 동일한 기능을 수행하는 3개의 thread를 실행하며, 이때, 
+     실행되는 각 thread의 argument로는 임의의 integer 값(이 값을 a라 한다.)을 기술한다.
+    수행되는 각 thread는 argument로 입력된 integer 값에 3을 더한 값을 pthread_exit 함수의 파라메타로 기술한다.
+    main thread는 자신이 수행한 모든 thread들이 종료하기를 기다리며, 각 thread가 종료하면 종료한 thread가 pthread_exit에서 기술한 값을 출력한다. (결국 a+3 값이 출력된다.)*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
