1e91adc560f2bd9cf8e5215c24d9fd2d2b01c609
1000hyrm <90028687+1000hyrm@users.noreply.github.com>
2022-06-14 16:29:06 +0900

    Update ex_18_3.c

--- ex_18_3.c
+++ ex_18_3.c
@@ -1,3 +1,15 @@
+/*  사용자가 n개의 argv를 입력하면, (n-1)개의 thread가 만들어진다.
+- 예: “./a.out 1 5 9”를 수행하면 3개의 thread가 생성된다.
+    각 스레드의 argument는 argv[i]가 되며,
+- 1번 thread의 시작 argument는 “1” (문자열)
+- 2번 thread의 시작 argument는 “5” (문자열)
+- 3번 thread의 시작 argument는 “9” (문자열)
+    각 thread는 시작하면 “sleep (1)”을 수행한 후, 수행 시작시 argument 값(int)의 제곱 값을 화면에 출력한다.
+- 1번 thread는 1초 sleep 후에 1을 출력한 후 종료
+- 2번 thread는 1초 sleep 후에 25를 출력한 후 종료
+- 3번 thread는 1초 sleep 후에 81를 출력한 후 종료
+    main thread는 생성한 모든 thread가 종료한 후 종료한다. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
