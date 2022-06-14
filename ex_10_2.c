c23f8a7e0e5c3a332742832ebe9957252ac382c7
1000hyrm <90028687+1000hyrm@users.noreply.github.com>
2022-06-14 14:19:56 +0900

    Update ex_10_2.c

--- ex_10_2.c
+++ ex_10_2.c
@@ -1,7 +1,7 @@
+
 //무한 루프를 돌면서, 사용자가 standard input(키보드)로부터 입력한 내용을 standard output(화면)에 출력하는 프로그램
 /*수행 도중에 사용자가 CTRL+c를 눌러서 SIGINT 시그널을 발생 시키면, 화면에 "SIGINT"라고 출력하고 종료하지 않고 하던 일을 계속한다.
 또한, 사용자가 CTRL+\를 눌려서 SIGQUIT 시그널을 발생시키면 화면에 "SIGQUIT"라고 출력하고 종료하도록 한다.*/
-
 
 #include <stdio.h>
 #include <stdlib.h>
